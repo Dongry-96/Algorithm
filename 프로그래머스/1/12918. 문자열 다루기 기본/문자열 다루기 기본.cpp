d3c6280373afb3fe8823623a96414688fe142fd5
Dongry-96 <158439321+Dongry-96@users.noreply.github.com>
2024-12-24 21:29:58 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.24 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -1,21 +1,21 @@
 #include <string>
 #include <vector>
+#include <cctype>
 
 using namespace std;
 
 bool ConditionCheck(string s)
 {
     int sLen = s.size();
-    // 글자수 체크
-    if(!(sLen == 4 || sLen == 6)) return false;
-    
+        
     // 숫자로만 구성인지 체크
     for(int i = 0; i < sLen; ++i)
     {
-        if(!('0' <= s[i] && s[i] <= '9')) return false;
+        if(!(isdigit(s[i]))) return false;
     }
-    
-    return true;
+           
+    // 글자수 체크
+    return (sLen == 4 || sLen == 6) ? true : false;
 }
 
 bool solution(string s) {
