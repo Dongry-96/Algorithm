e9bab1cef661dbe34a831662b1fe145bbe93ff6a
Dongry-96 <158439321+Dongry-96@users.noreply.github.com>
2024-12-30 10:52:34 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -3,46 +3,25 @@
 
 using namespace std;
 
-string ChangeString(string Target)
+string solution(string s) 
 {
-    for(int i = 0; i < Target.length(); i++)
+    string Answer = "";
+    int t = 1;
+    
+    for(int i = 0; i < s.length(); i++, t++)
     {
-        if(i % 2 == 0)
+        if(s[i] ==' ')
         {
-            if(Target[i] >= 'a' && Target[i] <= 'z')
-            {
-                char ch = Target[i] - 32;
-                Target.erase(i, 1);
-                Target.insert(i, 1, ch);
-            }
+            Answer += " ";
+            t = 0;
         }
         else
         {
-            if(Target[i] >= 'A' && Target[i] <= 'Z')
-            {
-                char ch = Target[i] + 32;
-                Target.erase(i, 1);
-                Target.insert(i, 1, ch);
-            }
+            //비트 And 연산자를 활용해서 홀수번째인지, 짝수번째인지 확인
+            //toupper함수와 tolower함수를 사용해서 대소문자 변환
+            t & 1 ? Answer += toupper(s[i]) : Answer += tolower(s[i]);
         }
     }
-    return Target;
-}
-
-string solution(string s) 
-{
-    int t = 0;
-    string Answer = "";
-    s += " ";
-    
-    while(s.find(" ", t) != -1)
-    {
-        string Target = s.substr(t, s.find(" ", t) - t);
-        Answer += ChangeString(Target) + " ";
-        t = s.find(" ", t) + 1;
-    }
-    
-    Answer.erase(Answer.length() - 1);
     
     return Answer;
 }
