aa096a2bc6219f646f96249373138d505511bc95
Dongry-96 <158439321+Dongry-96@users.noreply.github.com>
2025-01-07 15:50:55 +0900

    [level 1] Title: 과일 장수, Time: 31.98 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -5,39 +5,21 @@
 
 using namespace std;
 
-void check_apple(unordered_map<int, int>& List, int k, const vector<int>& score)
-{
-    for (int i = 1; i <= k; ++i)
-    {
-        int k_cnt = count(score.begin(), score.end(), i);
-
-        List[i] = k_cnt;
-    }
-}
 
 int solution(int k, int m, vector<int> score)
 {
-    unordered_map<int, int> apple_cnt;
     int result = 0;
+    // score 내림차순으로 정렬
+    sort(score.begin(), score.end(), greater<>());
 
-    // score 점수별로 사과가 몇 개 있는지 정리
-    check_apple(apple_cnt, k, score);
-
-    // k점수부터 1점수까지 m개씩 포장해서 점수 계산
-    for (int i = k; i >= 1; --i)
+    // i는 m씩 증가
+    for(int i = 0; i + m <= score.size(); i += m)
     {
-        // 만약 개수가 m보다 작다면 밑에 점수 개수에 저장
-        if (apple_cnt[i] < m)
-        {
-            apple_cnt[i - 1] += apple_cnt[i];
-            continue;
-        }
+        // 내림차순 정렬했기 때문에 m번째 요소가 박스에 들어갈 최소 점수
+        int min_score = score[i + m - 1];
 
-        // m으로 나눈 몫 * 점수를 result에 더해 저장
-        result += (apple_cnt[i] / m) * i * m;
-
-        // 나머지를 밑에 점수 개수에 저장
-        apple_cnt[i - 1] += apple_cnt[i] % m;
+        // 최소 점수 * 개수를 저장
+        result += min_score * m;
     }
 
 
