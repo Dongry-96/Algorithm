b1d327bdf766ba986af667c3a3626d918d39d7c1
Dongry-96 <158439321+Dongry-96@users.noreply.github.com>
2025-01-07 12:05:57 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -7,26 +7,18 @@ string solution(vector<string> cards1, vector<string> cards2, vector<string> goa
 {
     int one_idx = 0;
     int two_idx = 0;
-    int flag = 0;
 
-    int one_maxIdx = cards1.size() - 1;
-    int two_maxIdx = cards2.size() - 1;
     for(int i = 0; i < goal.size(); ++i)
     {
-        flag = 0;
-        
         if(goal[i] == cards1[one_idx])
         {
-            if(one_maxIdx > one_idx) one_idx++;
-            flag = 1;
+            one_idx++;
         }
         else if(goal[i] == cards2[two_idx])
         {
-            if(two_maxIdx > two_idx) two_idx++;
-            flag = 1;
+            two_idx++;
         }
-        
-        if(flag == 0)
+        else
         {
             return "No";
         }
