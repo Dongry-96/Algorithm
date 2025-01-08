21571754d0ea013bfa2bb4ef1ae7af106d360db9
Dongry-96 <158439321+Dongry-96@users.noreply.github.com>
2025-01-08 12:18:21 +0900

    [level 1] Title: 덧칠하기, Time: 0.36 ms, Memory: 5.52 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
@@ -3,28 +3,18 @@
 
 using namespace std;
 
-int solution(int n, int m, vector<int> section)
+int solution(int n, int m, vector<int> section) 
 {
-    vector<int> wall(n);
-    int cnt = 0;
-
-    for (int i = 0; i < section.size(); ++i)
+    int answer = 1;
+    int pivot = section[0];
+    
+    for(const auto& i : section)
     {
-        wall[section[i] - 1] = 1;
-    }
-
-    for (int i = 0; i < wall.size(); ++i)
-    {
-        if (wall[i] == 1)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                if (wall.size() > i + j) wall[i + j] = 0;
-            }
-            cnt++;
-        }
+        if(i < pivot + m) continue;
         
+        pivot = i;
+        answer++;
     }
-
-    return cnt;
+    
+    return answer;
 }
