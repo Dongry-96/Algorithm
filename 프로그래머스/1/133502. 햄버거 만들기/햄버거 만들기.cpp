fa640edd195b8abdc6b18c176ca360be84720929
Dongry-96 <158439321+Dongry-96@users.noreply.github.com>
2025-01-20 09:48:00 +0900

    [level 1] Title: 햄버거 만들기, Time: 12.28 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cpp
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cpp
@@ -1,16 +1,20 @@
-#include <string>
 #include <vector>
+#include <stack>
 using namespace std;
 
-int solution(vector<int> ingredient) {
+int solution(vector<int> ingredient) 
+{
     int answer = 0;
-    vector<int> v = { -1 };
-    for(int x : ingredient){
-        if(v.back() == 1 && x == 2) v.back() = 12;
-        else if(v.back() == 12 && x == 3) v.back() = 123;
-        else if(v.back() == 123 && x == 1) answer++, v.pop_back();
-        else v.push_back(x);
-    }    
+    stack<int> check_list;
+    check_list.push(-1);
+
+    for (int i : ingredient)
+    {
+        if (check_list.top() == 1 && i == 2) check_list.top() = 12;
+        else if (check_list.top() == 12 && i == 3) check_list.top() = 123;
+        else if (check_list.top() == 123 && i == 1) answer++, check_list.pop();
+        else check_list.push(i);
+    }
 
     return answer;
 }
