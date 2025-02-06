b51a7432dd6d6dea4473d78933553dd463ead60f
Dongry-96 <158439321+Dongry-96@users.noreply.github.com>
2025-02-06 12:49:09 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 99.08 ms, Memory: 20.4 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -1,44 +1,26 @@
 #include <string>
 #include <vector>
 #include <unordered_set>
-#include <deque>
-#include <numeric> // accumulate
 
 using namespace std;
 
-int solution(vector<int> elements)
+int solution(vector<int> elements) 
 {
-    // 합산 결과를 키로 저장. 중복 x
     unordered_set<int> us;
+    int size = elements.size();
 
-    // 원형 순열로 모든 요소가 합산의 시작점이 될 수 있다.
-    // 양쪽 끝에서의 삽입/삭제가 빠른 deque 컨테이너 사용.
-    deque<int> dq(elements.begin(), elements.end());
-    
-    // 더할 숫자 개수
-    int cnt = 1;
-    while (cnt != elements.size())
+    for (int start = 0; start < size; ++start)
     {
-        // elements의 크기만큼 반복
-        // 원형 순열이기에 덧셈 시작 위치를 모든 요소에 적용
-        for (int i = 0; i < elements.size(); ++i)
+        int sum = 0;
+        for (int current = start; current < start + size; ++current)
         {
-            // dq의 시작부터 cnt 개수만큼 더한 값을 set에 저장
-            us.insert(accumulate(dq.begin(), dq.begin() + cnt, 0));
-
-            // 원형 순열이기 때문에 dq.front 시작점 갱신
-            int temp = dq.front();
-            dq.pop_front();
-            dq.push_back(temp);
+            // 모듈러 연산을 통해 vector size 범위 초과하지 않도록 함.
+            // current가 vector size 값이 되면 0번 인덱스가 된다.
+            sum += elements[current % size];
+            us.insert(sum);
         }
-        // 더할 갯수 증가
-        cnt++;
     }
-    
-    // 모든 요소를 더할 때는 시작점이 중요하지 않기에 한 번만 저장
-    us.insert(accumulate(dq.begin(), dq.begin() + cnt, 0));
-    
-    // set의 크기가 곧 합산의 경우의 수
+
     int answer = us.size();
     return answer;
 }
