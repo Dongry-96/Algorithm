afba821f4a1d287c599b0fd9bb1e78eb555b98e1
Dongry-96 <158439321+Dongry-96@users.noreply.github.com>
2025-04-24 09:59:04 +0900

    [level 2] Title: 소수 찾기, Time: 1.18 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -2,14 +2,13 @@
 #include <vector>
 #include <unordered_set>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
 bool isprimenum(const string& str)
 {
     int num = stoi(str);
-
-    if (num < 2) return false;
 
     for (int i = 2; i <= sqrt(num); ++i)
     {
@@ -19,29 +18,25 @@ bool isprimenum(const string& str)
     return true;
 }
 
-void DFS(const string& numbers, string str, unordered_set<int>& prime_num, vector<bool>& used)
-{
-    if (!str.empty())
-    {
-        if (isprimenum(str)) prime_num.insert(stoi(str));
-    }
-
-    for (int i = 0; i < numbers.length(); ++i)
-    {
-        if (used[i]) continue;
-
-        used[i] = true;
-        DFS(numbers, str + numbers[i], prime_num, used);
-        used[i] = false;
-    }
-}
-
 int solution(string numbers)
 {
     unordered_set<int> prime_num;
-    vector<bool> used(numbers.length(), false);
+    sort(numbers.begin(), numbers.end());
 
-    DFS(numbers, "", prime_num, used);
+    do
+    {
+        for (int len = 1; len <= numbers.length(); ++len)
+        {
+            string str = numbers.substr(0, len);
+
+            if (stoi(str) < 2) continue;
+
+            if (prime_num.find(stoi(str)) == prime_num.end())
+            {
+                if (isprimenum(str)) prime_num.insert(stoi(str));
+            }
+        }
+    } while (next_permutation(numbers.begin(), numbers.end()));
 
     return prime_num.size();
 }
